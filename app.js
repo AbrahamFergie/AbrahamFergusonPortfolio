73919e0bc00381724134ec3f373d8faff9cadfaf
Abraham Ferguson <abrahamferguson@Abrahams-MacBook-Pro.local>
2017-05-24 10:29:47 -0700

    configuring app for deployment

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+
+const port = process.env.PORT || 3000
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function(req,res) {
+  res.sendFile('index.html')
+})
+app.set('port', (process.env.PORT || 3000))
+var server = app.listen(app.get('port'), function() {
+  server.address().port
+  console.log("=====listening on port 3000!!=====")
+})
